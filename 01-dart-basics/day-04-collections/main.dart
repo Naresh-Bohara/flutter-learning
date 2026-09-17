254a681c6922e964b0b6892a307297db4ef68562
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-09-17 21:15:07 +0545

    updated upto day 4

---
+++ 01-dart-basics/day-04-collections/main.dart
@@ -0,0 +1,25 @@
+void main() {
+  // List: ordered collection of items.
+
+  List<String> name = ["Ram", "Hari", "Rohan"];
+  List<int> nums = [12, 23, 34, 45, 65];
+  List<dynamic> dynamicData = [12, 23, 34, 45, 65, "hari", "roshan", 2.33];
+  // print(name);
+  // print(nums.length);
+  // print(dynamicData);
+  // nums.add(444);
+  // print(nums);
+  // nums.insert(2, 555);
+  // print(nums);
+  // nums.addAll([1,2,33,4,5,5,56,7]);
+  // print(nums);
+  // nums.insertAll(1, [12,3,4,5,6,]);
+  // nums.remove(1);
+  // nums.clear();
+
+  // update
+  // nums[0] = 122;
+
+ // set:
+  
+}
